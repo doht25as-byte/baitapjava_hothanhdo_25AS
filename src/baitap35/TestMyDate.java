b649ff0d201ec31e3d5487a60017cd6f3c1e3fed
doht25as-byte <doht.25as@vku.udn.vn>
2025-11-07 08:22:41 +0700

    baitap3.5

--- src/baitap35/TestMyDate.java
+++ src/baitap35/TestMyDate.java
@@ -4,7 +4,6 @@ public class TestMyDate {
     public static void main(String[] args) {
         MyDate d1 = new MyDate(2012, 2, 14);
         System.out.println(d1); // Tuesday 14 Feb 2012
-
         d1.nextDay();
         System.out.println("Next Day: " + d1);
 
