f8a9ae5b5ff99243fe73a887f7b052730a47e299
hothanhdo <doht.25as@vku.udn.vn>
2026-01-02 11:11:30 +0700

    baitap6.8

---
+++ src/baitap68/TestMovable.java
@@ -0,0 +1,20 @@
+package baitap68;
+
+public class TestMovable {
+    public static void main(String[] args) {
+
+        MovablePoint p = new MovablePoint(1, 2, 3, 4);
+        System.out.println(p);
+        p.moveRight();
+        p.moveDown();
+        System.out.println("After moving point: " + p);
+
+        System.out.println();
+
+        MovableCircle c = new MovableCircle(5, 5, 2, 2, 10);
+        System.out.println(c);
+        c.moveLeft();
+        c.moveUp();
+        System.out.println("After moving circle: " + c);
+    }
+}
